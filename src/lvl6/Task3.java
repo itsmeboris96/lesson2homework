14d15c0b3a49b5554bcd7547d5b450200bcd82e8
bormas <borisnoteprog@gmail.com>
2025-02-02 16:18:20 +0300

    complete lesson 2

---
+++ src/lvl6/Task3.java
@@ -0,0 +1,23 @@
+package lvl6;
+
+/**
+ * Заставьте эту программу работать!
+ *
+ * class AttentionPlease {
+ *
+ *     System.out.println("Code outside the method will not compile!");
+ *
+ *     public static void main(String[] args) {
+ *
+ *     }
+ * }
+ *
+ * Цель этого задания: обратить ваше внимания на то, где должен
+ * располагаться программный код. В языке программирования Java
+ * программный код с командами должен находиться внутри метода.
+ */
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
---
+++ src/lvl7/Task1.java
@@ -0,0 +1,22 @@
+package lvl7;
+
+/**
+ * Заставьте эту программу работать!
+ * <p>
+ * class NoSleepNoFocus {
+ * public static void main(String[] args) {
+ * System.out.println(I wanna coffee);
+ * System.out.println("So bad);
+ * }
+ * }
+ * <p>
+ * Цель этого задания: обратить ваше внимание на то,
+ * что текст (строка) в языке программирования Java
+ * пишется в двойных кавычках.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
